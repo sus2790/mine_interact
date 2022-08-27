6afd37597c232dd16a9646112c999880c49cbfb0
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-08-27 18:48:40 +0800

    Update data.py

--- mine_interact/data.py
+++ mine_interact/data.py
@@ -1,18 +1,23 @@
-from typing import List
-
+from typing import (
+    Optional,
+    List,
+)
 from dataclasses import dataclass
 
+__all__ = (
+    'User',
+)
 
 @dataclass(frozen=True)
 class User:
-    id: int
-    pickaxe: int
-    backpack: str
-    money: int
-    field: str
-    exp: int
-    level: int
-    mine_count: int
-    guild: str
-    mystery_stone: int
-    skill: List
+    id: Optional[int]
+    pickaxe: Optional[int]
+    backpack: Optional[str]
+    money: Optional[int]
+    field: Optional[str]
+    exp: Optional[int]
+    level: Optional[int]
+    mine_count: Optional[int]
+    guild: Optional[str]
+    mystery_stone: Optional[int]
+    skill: Optional[List]
