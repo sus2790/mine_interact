2a4fc2be98cccf42688461da2932c293a28a0d2c
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-08-27 23:59:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="mine_interact",
-    version="1.0.0",
+    version="1.0.0b,
     packages=["mine_interact"],
     license="MIT license",
     long_description="用於 MineBot 交互的 Package",
-    author="cutebear",
-    author_email="cutebear@bear-team.dev",
+    author="``#2790",
+    author_email="amengpan6@gmail.com",
 )
