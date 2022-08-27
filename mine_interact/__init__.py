1f0865252ee9b0df554f5ab48a016fab97ad77da
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-08-27 18:47:58 +0800

    Update __init__.py

--- mine_interact/__init__.py
+++ mine_interact/__init__.py
@@ -1,1 +1,12 @@
-from .mine import Mine
+'''
+Mine-Interact
+
+用於Mine Bot交互的 Package
+'''
+
+__version__ = 'v1.0'
+__author__ = 'cutebear'
+
+from .mine import *
+from .data import *
+from .errors import *
