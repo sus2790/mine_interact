657061e9180b67b7614ba6e9f35c81f3796f781f
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-08-28 15:54:32 +0800

    FIX THE VERISON

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mine_interact",
-    version="1.0.0b,
+    version="1.0.0b",
     packages=["mine_interact"],
     license="MIT license",
     long_description="用於 MineBot 交互的 Package",
