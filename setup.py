861c7312aafb349df7dfc7b098986b15404de380
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-09-01 20:43:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+packages=["mine_interact"]
+
 setup(
     name="mine_interact",
-    version="1.0.0b",
-    packages=["mine_interact"],
-    license="MIT license",
-    long_description="用於 MineBot 交互的 Package",
     author="``#2790",
     author_email="amengpan6@gmail.com",
+    version="1.0.1",
+    packages=packages,
+    license="MIT",
+    description="用於 Mine Bot 交互的 Package",
+    long_description="Mine Interact is a Python libary for Mine Bot Interact.",
+    python_requires=">=3.8.0",
 )
